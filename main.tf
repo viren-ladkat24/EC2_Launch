5823f89fd34ae92ae7c484c625fdf467dd3af758
viren ladkat <virenladkat94@gmail.com>
2026-01-05 15:23:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
-provider "aws" {
-  region = "ap-south-1"
+resource "aws_instance" "ec2" {
+  ami           = "ami-00ca570c1b6d79f36"  # Amazon Linux 2 (Mumbai)
+  instance_type = "t2.micro"
+  key_name      ="serverkey"
+
+
+  tags = {
+    Name = "terraform-ec2"
+  }
 }
